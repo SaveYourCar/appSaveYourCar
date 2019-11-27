03a97345276b6d773c97e89dc3a1346dc6c96722
luca-A11 <lucagsls12@gmail.com>
2019-11-27 12:40:21 +0100

    Division Model Users Tables

--- app/models/modelUsers.py
+++ app/models/modelUsers.py
@@ -11,7 +11,12 @@ def load_user(user_id):
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-    password = db.Column(db.String(60), nullable=False)
     cars = db.relationship('Car', backref='author', lazy=True)
+    user_login = db.relationship('UserLogin', backref='owner', lazy=True)
+
+class UserLogin(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    id_user = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
