7a75be19f608e2e6d07a45486b20994d57320eb1
filippo <basciu45@gmail.com>
2019-11-27 10:06:01 +0100

    add settings

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from app.settings.config import SECRET_KEY
+from app.settings.configDB import SQLALCHEMY_DATABASE_URI
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SECRET_KEY'] = SECRET_KEY
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 db = SQLAlchemy(app)
 bcrypt = Bcrypt()
 login_manager = LoginManager()
