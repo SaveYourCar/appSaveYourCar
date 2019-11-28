0e44fbfd92c720b8b4b4f249f65bcb784f5ab32a
Ivana <ivana.zaccheddu88@gmail.com>
2019-11-28 16:27:16 +0100

    solve problem log in and function insertCarDataValue

--- app/controllers/controllerUsers.py
+++ app/controllers/controllerUsers.py
@@ -31,8 +31,8 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         userLogin = UserLogin.query.filter_by(email=form.email.data).first()
-        user = User.query.filter_by(id=userLogin.id_user).first()
-        if user and bcrypt.check_password_hash(userLogin.password, form.password.data):
+        user = user = User.query.filter_by(id=userLogin.id).first()
+        if userLogin and bcrypt.check_password_hash(userLogin.password, form.password.data):
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
             return redirect(next_page) if next_page else redirect(url_for('main.home'))
--- app/controllers/utilsCars.py
+++ app/controllers/utilsCars.py
@@ -1,17 +1,24 @@
 from app.models.modelCars import Car, CarDataValue, CarData
 from app import db
 
+
 def AddCarValueToDb(car,form):
-	lista = [form.kmattuali.data, form.dataRevisione.data,
-             form.kmTagliando.data, form.dataAssicurazione.data,
-             form.dataBollo.data, form.kmMedi.data]
-	carDataValue={}
-	for i in range(1,7):
-		if i == 1 or 3 or 6:
-			carDataValue[i] = CarDataValue(valueInt = lista[i-1],
-                                           id_CarData = i, car_author = car )
-			db.session.add(carDataValue[i])
-		elif i == 2 or 4 or 5:
-			carDataValue[i] = CarDataValue(valueDate = lista[i-1],
-                                           id_CarData = i, car_author = car )
-			db.session.add(carDataValue[i])
+	carValue1 = CarDataValue(valueInt = form.kmattuali.data,
+                                           id_CarData = 1, car_author = car )
+	db.session.add(carValue1)
+	carValue2 = CarDataValue(valueDate = form.dataRevisione.data,
+                                           id_CarData = 2, car_author = car )
+	db.session.add(carValue2)
+	carValue3 = CarDataValue(valueInt = form.kmTagliando.data,
+                                           id_CarData = 3, car_author = car )
+	db.session.add(carValue3)
+	carValue4 = CarDataValue(valueDate = form.dataAssicurazione.data,
+                                           id_CarData = 4, car_author = car )
+	db.session.add(carValue4)
+	carValue5 = CarDataValue(valueDate = form.dataBollo.data,
+                                           id_CarData = 5, car_author = car )
+	db.session.add(carValue5)
+	carValue6 = CarDataValue(valueInt = form.kmMedi.data,
+                                           id_CarData = 6, car_author = car )
+	db.session.add(carValue6)
+		
